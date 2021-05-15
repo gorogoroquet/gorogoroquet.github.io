1dd3774b8bf95cf36cb97ab513b15b0a383b7885
gorogoroquet <83847860+gorogoroquet@users.noreply.github.com>
2021-05-15 12:01:55 +0900

    hungry
    hungry

---
+++ index.js
@@ -0,0 +1,58 @@
+window.onload = function () {
+    var desc = ["so hungy",
+    "really really hungry",
+    "plz someone take me to subway"]; 
+    var index = 0;
+
+    var interval = setInterval(function() {
+        $('#book1_desc').fadeOut(500, function() {
+            index++;
+            if (index === desc.length)
+                index = 0;
+            $(this).text(desc[index]).fadeIn(500);
+        });
+    }, 4000);
+
+    var leftarrow = document.getElementById('left-arrow');
+    var rightarrow = document.getElementById('right-arrow');
+
+    leftarrow.onclick = function () {
+        $('#book1_desc').fadeOut(500,function() {
+           index --;
+           if (index === -1)
+              index = 2;
+           $(this).text(desc[index]).fadeIn(500);
+          clearInterval(interval);
+          interval = setInterval(function() {
+            $('#book1_desc').fadeOut(500, function() {
+                index++;
+                if (index === desc.length)
+                    index = 0;
+                $(this).text(desc[index]).fadeIn(500);
+            });
+        }, 4000);
+    
+    })
+ };
+
+
+rightarrow.onclick = function () {
+    $('#book1_desc').fadeOut(500,function() {
+       index ++;
+       if (index === desc.length)
+          index = 0;
+       $(this).text(desc[index]).fadeIn(500);
+      clearInterval(interval);
+      interval = setInterval(function() {
+        $('#book1_desc').fadeOut(500, function() {
+            index++;
+            if (index === desc.length)
+                index = 0;
+            $(this).text(desc[index]).fadeIn(500);
+        });
+    }, 4000);
+
+   })
+  };
+
+}
