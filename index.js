f78a722f9adaaa639754a548b9dd2c3bb29a8906
gorogoroquet <83847860+gorogoroquet@users.noreply.github.com>
2021-05-29 12:06:24 +0900

    commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 window.onload = function () {
-    var desc = [document.getElementById('book1_desc').innerText, "to make does rainbow in my mind",
-    "just the to of us, we can make if we try",
-    "wish you were again"]; 
+    var desc = [document.getElementById('book1_desc').innerText,
+    "문산수억고등학교 웹페이지 멘토링입니다.",
+    "잘 부탁드립니다!"]; // 문자열 목록
     var index = 0;
 
     var interval = setInterval(function() {
         $('#book1_desc').fadeOut(500, function() {
-            index++;
+            // 지금 표시된 문자가 사라지면 수행할 작업
+            index++; // 0 >> 1
             if (index === desc.length)
                 index = 0;
             $(this).text(desc[index]).fadeIn(500);
@@ -18,41 +19,47 @@ window.onload = function () {
 
     leftarrow.onclick = function () {
         $('#book1_desc').fadeOut(500,function() {
-           index --;
-           if (index === -1)
-              index = 2;
-           $(this).text(desc[index]).fadeIn(500);
-          clearInterval(interval);
-          interval = setInterval(function() {
-            $('#book1_desc').fadeOut(500, function() {
-                index++;
-                if (index === desc.length)
-                    index = 0;
-                $(this).text(desc[index]).fadeIn(500);
-            });
-        }, 4000);
-    
-    })
- };
+            index--;
+            if (index === -1)
+                index = 2;
+            $(this).text(desc[index]).fadeIn(500);
+            clearInterval(interval); // 문자열 자동변환 주기를 초기화
+            interval = setInterval(function() {
+                $('#book1_desc').fadeOut(500, function() {
+                    // 지금 표시된 문자가 사라지면 수행할 작업
+                    index++; // 0 >> 1
+                    if (index === desc.length)
+                        index = 0;
+                    $(this).text(desc[index]).fadeIn(500);
+                });
+            }, 4000);
+        })
+    }
 
-
-rightarrow.onclick = function () {
-    $('#book1_desc').fadeOut(500,function() {
-       index ++;
-       if (index === desc.length)
-          index = 0;
-       $(this).text(desc[index]).fadeIn(500);
-      clearInterval(interval);
-      interval = setInterval(function() {
-        $('#book1_desc').fadeOut(500, function() {
+    rightarrow.onclick = function () {
+        $('#book1_desc').fadeOut(500,function() {
             index++;
             if (index === desc.length)
                 index = 0;
             $(this).text(desc[index]).fadeIn(500);
-        });
-    }, 4000);
+            clearInterval(interval); // 문자열 자동변환 주기를 초기화
+            interval = setInterval(function() {
+                $('#book1_desc').fadeOut(500, function() {
+                    // 지금 표시된 문자가 사라지면 수행할 작업
+                    index++; // 0 >> 1
+                    if (index === desc.length)
+                        index = 0;
+                    $(this).text(desc[index]).fadeIn(500);
+                });
+            }, 4000);
+        })
+    }
 
-   })
-  };
+
+    
+
+    // 문자열은 모두 따옴표를 사용해서 입력 "문자열 어쩌구저쩌구"
+    // 문자열끼리는 쉼표(,)로 구분
+    // 배열 대괄호[ ] 꼭 열고 닫아주기!
 
 }
