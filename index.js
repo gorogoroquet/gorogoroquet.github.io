d54d81d43849711721a853a739df4295df6ea336
gorogoroquet <83847860+gorogoroquet@users.noreply.github.com>
2021-06-05 12:04:40 +0900

    bubblepop

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var diary_num = 3;
+var diary_idx = 1;
+
 window.onload = function () {
     var desc = [document.getElementById('book1_desc').innerText,
     "문산수억고등학교 웹페이지 멘토링입니다.",
@@ -62,4 +65,47 @@ window.onload = function () {
     // 문자열끼리는 쉼표(,)로 구분
     // 배열 대괄호[ ] 꼭 열고 닫아주기!
 
+    replaceDiary('/diary/diary1.html')
+
+    var diary_left_arrow = document.getElementById("diary_left_arrow");
+    var diary_right_arrow = document.getElementById("diary_right_arrow");
+
+    diary_left_arrow.onclick = diaryLeftClick;
+    diary_right_arrow.onclick = diaryRightClick;
+}
+
+function replaceDiary(filename) {
+    $.get(filename, function(result){
+        var obj = $(result).find('p');
+        document.getElementById("diary_text").innerText = obj.html();
+    });
+         $.get(filename, function(result){
+            var obj = $(result).find('h2');
+            document.getElementById("diary_title").innerText = obj.html();
+        });
+        $.get(filename, function(result){
+            var obj = $(result).find('img');
+            document.getElementById("diary_img").src = obj[0].src;
+        });
+}
+
+function diaryLeftClick(){
+    $('.diary').fadeOut(500, function(){
+        diary_idx--;
+        if (diary_idx < 1) diary_idx = diary_num;
+        var html = "/diary/diary" + diary_idx + ".html";
+        replaceDiary(html);
+        $(this).fadeIn(500);
+    });
+}
+
+
+function diaryRightClick(){
+    $('.diary').fadeOut(500, function(){
+        diary_idx++;
+        if (diary_idx > diary_num) diary_idx = 1;
+        var html = "/diary/diary" + diary_idx + ".html";
+        replaceDiary(html);
+        $(this).fadeIn(500);
+    });
 }
